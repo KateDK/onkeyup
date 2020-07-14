d2a861a98f1acdacfe449c92de88113b532a9348
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-14 14:28:55 -0400

    Create WizdomSquare component

---
+++ src/WizdomSquare.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class WizdomSquare extends React.Component {
+  render(){
+    return (
+    <div className='WizdomSquare'>
+      HI!
+    </div>
+    )
+  }
+}
+export default WizdomSquare;
