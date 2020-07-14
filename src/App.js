2cfac1afad98bc5c4492f491a1645ce29724bb4d
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-14 14:29:08 -0400

    render WizdomSquare

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import WizdomSquare from './WizdomSquare'
 function App() {
   return (
     <div className="App">
-Hello
+      <WizdomSquare/>
     </div>
   );
 }
