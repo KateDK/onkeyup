fa7fe3590ecbc288c261d368bd6d7b45400478eb
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-16 16:28:49 -0400

    fix spelling in wisdom.js

--- src/wisdom.js
+++ src/wisdom.js
@@ -1,3 +1,3 @@
-let wizdom = ["It is better to remain silent at the risk of being thought a fool, than to talk and remove all doubt of it.","The fool doth think he is wise, but the wise man knows himself to be a fool.","Knowing yourself is the beginning of all wisdom.","The only true wisdom is in knowing you know nothing.","May you live every day of your life."];
+let wisdom = ["It is better to remain silent at the risk of being thought a fool, than to talk and remove all doubt of it.","The fool doth think he is wise, but the wise man knows himself to be a fool.","Knowing yourself is the beginning of all wisdom.","The only true wisdom is in knowing you know nothing.","May you live every day of your life."];
 
-export default wizdom;
+export default wisdom;
