651e768f63cfbc11eb4a48476be26cfb97620ed7
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-16 16:29:32 -0400

    create randomWisdom method

--- src/WisdomSquare.js
+++ src/WisdomSquare.js
@@ -2,6 +2,9 @@ import React from 'react';
 import wisdom from './wisdom';
 
 class WisdomSquare extends React.Component {
+  randomWisdom = () => {
+    return wisdom[Math.floor(Math.random()*wisdom.length)]
+  }
   render(){
     return (
     <div className='WisdomSquare'>
