3cc44e9925b5efe3ab691d9917e037d8bb830b14
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-20 12:08:57 -0400

    Create WisdomForm component

---
+++ src/WisdomForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class WisdomForm extends React.Component{
+  render(){
+    return(
+      <div className='WisdomForm'>
+        I am a proud form
+      </div>
+    )
+  }
+}
+
+export default WisdomForm;
