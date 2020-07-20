c3b269bb9ae8b9b26e43fb8eb767f6978f8f1fe3
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-20 12:10:32 -0400

    Replace WisdomSquare with Container in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import WisdomSquare from './WisdomSquare'
+import Container from './Container';
+
 function App() {
   return (
     <div className="App">
-      <WisdomSquare/>
+      <Container/>
     </div>
   );
 }
