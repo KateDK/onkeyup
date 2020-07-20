d48ecce0970291500409e5b818f012f093b1eca6
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-20 12:10:00 -0400

    Create Container component
    Import WisdomSquare and WisdomForm into it.

---
+++ src/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WisdomSquare from './WisdomSquare';
+import WisdomForm from './WisdomForm';
+
+const Container = ()=>(
+  <div className='Container'>
+    <WisdomSquare/>
+    <WisdomForm />
+  </div>
+);
+
+export default Container;
