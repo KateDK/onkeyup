a77bd72c91c6acedabaa6ff7baaa3589aeada597
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-20 15:48:14 -0400

    Add state and OnKeyUp event handler to WisdomForm

--- src/WisdomForm.js
+++ src/WisdomForm.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './WisdomForm.css';
 class WisdomForm extends React.Component{
+  state={
+    content:'',
+  }
+
+  handleKeyUp = (e) => {
+    this.setState({content: this.statecontent+e.key})
+
+  }
+
   render(){
     return(
       <div className='WisdomForm'>
-        <textarea placeholder="Share your own wisdom:"/>
+        <textarea placeholder="Share your own wisdom:" onKeyUp={this.handleKeyUp}/>
       </div>
     )
   }
