5521eb1f203886cfb8e6c46f63a2655726d58b29
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-21 14:58:49 -0400

    Add logic to update specialCount

--- src/WisdomForm.js
+++ src/WisdomForm.js
@@ -12,8 +12,14 @@ class WisdomForm extends React.Component{
     const {content,specialCount}= this.state;
     if(char >= 32 && char < 127 ){
       const key = e.key;
+      this.setState({content: content+key, specialCount: key === "*" ? specialCount+1 : specialCount, special: specialCount+1 > 0});
     }
     if(char === 8){
+      const last = content[content.length-1];
+      this.setState({content: content.slice(0,-1)});
+      if(last === "*"){
+        this.setState({specialCount: specialCount-1,special: specialCount-1 > 0})
+      }
     }
   }
 
