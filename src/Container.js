e70ec34938deb08555950194b55ac2ec274fef42
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-21 15:46:43 -0400

    import Copy into Container

--- src/Container.js
+++ src/Container.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import WisdomSquare from './WisdomSquare';
 import WisdomForm from './WisdomForm';
+import Copy from './Copy'
 
 const Container = ()=>(
   <div className='Container'>
     <WisdomSquare/>
     <WisdomForm />
+    <Copy/>
   </div>
 );
 
